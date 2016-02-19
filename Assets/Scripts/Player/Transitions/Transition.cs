d91d597f647d34e64c502dc6e28d765cc2dd6ceb
Jan van der Kamp <experimentalcoding@users.noreply.github.com>
2016-02-19 07:40:13 +0000

    Moved transitoin class source files into Transition folder

---
+++ Assets/Scripts/Player/Transitions/Transition.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Transition interface which different transition classes conform to.
+// Each transition instance is setup with a desired translation, rotation, or both.
+// Interpolate should then be called during Update or LateUpdate.
+public interface Transition
+{
+	bool Interpolate(float deltaTime);
+}
